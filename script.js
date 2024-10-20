a2b68053e8dd9b0c1238a9e739c513471abdad47
Neo Franco <neo.franco@august99.com>
2024-10-20 16:31:57 +0800

    Updated HTML markups, fixed CSS styling, and fixed responsive width

--- script.js
+++ script.js
@@ -2,17 +2,30 @@
 
     const app = {
         slick: function() {
-            const gallerySlider = $('.gallery-slider');
-            if (!gallerySlider.length) return;
+            const gallerySlider = $('.gallery-slider'),
+                  gallerySlides = gallerySlider.children();
+            
+            // Checks if the slider exists or if it has more than 1 child element
+            if (!gallerySlider.length || gallerySlides.length < 2) return; 
+            
+            // Checks if the slider contains less than 2 or equal to 3 child elements
+            if (gallerySlides.length <= 3 && gallerySlides.length > 1) {
+                const clonedChildren = gallerySlides.clone();
+                gallerySlider.append(clonedChildren);
+            }
 
             gallerySlider.on('init afterChange', function(event, slick, currentSlide) {
-                const slides = slick.$slides;
+                const slides = slick.$slides,
+                      defaultWidth = 400,
+                      activeWidth = 800,
+                      defaultWidthResp = `min(${defaultWidth}px, ${(defaultWidth/1600)*100}vw)`,
+                      activeWidthResp = `min(${activeWidth}px, ${(activeWidth/1600)*100}vw)`;
 
                 slides.each(function(index, element) {
-                    $(element).css('width', '400px');
+                    $(element).css('width', defaultWidthResp);
                     
                     if ($(element).hasClass('slick-current')) {
-                        $(element).css('width', '800px');
+                        $(element).css('width', activeWidthResp);
                     }
 
                     if ($(element).hasClass('slick-active')) {
@@ -25,8 +38,6 @@
 
                 // First occurences of slick-cloned after the visible slick-slide
                 $(slides[slides.length - 1]).next().addClass('is-cloned');
-
-
             }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
                 const slides = slick.$slides;
                 $('.slick-slide').removeClass('is-visible');
@@ -39,33 +50,19 @@
             });
 
             gallerySlider.slick({
+                autoplay: true,
                 slidesToShow: 1,
                 touchThreshold: 5,
                 variableWidth: true,
-                autoplay: true,
-                autoplaySpeed: 6000,
+                autoplaySpeed: 5000,
                 prevArrow: $('.gallery-arrow .prev'),
                 nextArrow: $('.gallery-arrow .next'),
-            })
-        },
-        splide: function() {
-            var splide = new Splide( '.splide', {
-                // drag: false,
-                type: 'loop',
-                autoWidth: true,
-            } );
-            
-            splide.on( 'moved', function (newIndex, prevIndex, destIndex) {
-                console.log(splide)
-            } );
-
-            splide.mount();
+            });
         },
     }
 
     $(window).ready(function() {
         app.slick();
-        app.splide();
     })
 
 })();
